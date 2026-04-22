8bfcc761695d3493ee2c10c549e00dd42a8e5422
yxxawa <2509788557@qq.com>
2026-04-22 13:54:47 +0800

    Delete Models directory

--- Models/AppState.cs
+++
@@ -1,8 +0,0 @@
-namespace Hanime1Downloader.CSharp.Models;
-
-public sealed class AppState
-{
-    public string CookieHeader { get; set; } = string.Empty;
-    public string BrowserVersion { get; set; } = string.Empty;
-    public List<BrowserCookieRecord> Cookies { get; set; } = [];
-}
